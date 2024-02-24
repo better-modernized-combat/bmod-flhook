487976be585cd9fa0f6ce053970d7804f0938dc4
Shiniri <mariebauer2610@gmail.com>
2024-02-24 15:39:26 +0100

    empty template

---
+++ plugins/crafting/Crafting.cpp
@@ -0,0 +1,36 @@
+﻿// Includes
+#include "Crafting.hpp"
+
+#include <random>
+
+namespace Plugins::Crafting
+{
+	const auto global = std::make_unique<Global>();
+
+	void LoadSettings()
+	{
+		// Load JSON config
+		auto config = Serializer::JsonToObject<Config>();
+		global->config = std::make_unique<Config>(std::move(config));
+	}
+
+} // namespace Plugins::Crafting
+
+using namespace Plugins::Crafting;
+
+REFL_AUTO(type(Config));
+
+DefaultDllMainSettings(LoadSettings);
+
+extern "C" EXPORT void ExportPluginInfo(PluginInfo* pi)
+{
+	// Full name of your plugin
+	pi->name("Crafting");
+	// Shortened name, all lower case, no spaces. Abbreviation when possible.
+	pi->shortName("crafting");
+	pi->mayUnload(true);
+	pi->returnCode(&global->returnCode);
+	pi->versionMajor(PluginMajorVersion::VERSION_04);
+	pi->versionMinor(PluginMinorVersion::VERSION_00);
+	pi->emplaceHook(HookedCall::FLHook__LoadSettings, &LoadSettings, HookStep::After);
+}
---
+++ plugins/crafting/Crafting.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <FLHook.hpp>
+#include <plugin.h>
+
+namespace Plugins::Crafting
+{
+	// Loadable json configuration
+	struct Config : Reflectable
+	{
+		std::string File() override { return "config/crafting.json"; }
+	};
+
+	struct Global
+	{
+		std::unique_ptr<Config> config = nullptr;
+		ReturnCode returnCode = ReturnCode::Default;
+	};
+} // namespace Plugins::Crafting
