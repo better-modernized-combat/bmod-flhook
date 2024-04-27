26e2c8099a472fa60aa7dd661b49ffdcc483a88c
Rupert Walker <RupertCW@gmail.com>
2024-04-27 13:27:03 +0100

    feat: Blank template for triggers

---
+++ plugins/triggers/Triggers.cpp
@@ -0,0 +1,34 @@
+﻿// Includes
+#include "Triggers.hpp"
+
+#include <random>
+
+namespace Plugins::Triggers
+{
+	const auto global = std::make_unique<Global>();
+
+	void LoadSettings()
+	{
+		// Load JSON config
+		auto config = Serializer::JsonToObject<Config>();
+		global->config = std::make_unique<Config>(std::move(config));
+	}
+
+} // namespace Plugins::Triggers
+
+using namespace Plugins::Triggers;
+
+REFL_AUTO(type(Config));
+
+DefaultDllMainSettings(LoadSettings);
+
+extern "C" EXPORT void ExportPluginInfo(PluginInfo* pi)
+{
+	pi->name("Triggers");
+	pi->shortName("triggers");
+	pi->mayUnload(true);
+	pi->returnCode(&global->returnCode);
+	pi->versionMajor(PluginMajorVersion::VERSION_04);
+	pi->versionMinor(PluginMinorVersion::VERSION_00);
+	pi->emplaceHook(HookedCall::FLHook__LoadSettings, &LoadSettings, HookStep::After);
+}
---
+++ plugins/triggers/Triggers.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <FLHook.hpp>
+#include <plugin.h>
+
+namespace Plugins::Triggers
+{
+	// Loadable json configuration
+	struct Config : Reflectable
+	{
+		std::string File() override { return "config/triggers.json"; }
+	};
+
+	struct Global
+	{
+		std::unique_ptr<Config> config = nullptr;
+		ReturnCode returnCode = ReturnCode::Default;
+	};
+} // namespace Plugins::Triggers
